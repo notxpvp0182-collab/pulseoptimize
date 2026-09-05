1b9a82845bfc5fc914b48b0dc88f6448f03871c4
Ahmad <notxpvp0182@gmail.com>
2026-09-05 08:03:57 +0600

    fix: KeyBinding.Category.create() takes Identifier in 1.21.11

--- src/main/java/dev/ahmad/pulseoptimize/util/KeybindManager.java
+++ src/main/java/dev/ahmad/pulseoptimize/util/KeybindManager.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeybindManager {
@@ -15,7 +16,7 @@ public class KeybindManager {
     private KeyBinding toggleAutoOptimize;
 
     private static final KeyBinding.Category KEYBIND_CATEGORY =
-            KeyBinding.Category.create("key.categories.pulseoptimize");
+            KeyBinding.Category.create(Identifier.of("pulseoptimize", "keybindings"));
 
     public void register() {
         toggleHud = KeyBindingHelper.registerKeyBinding(new KeyBinding(
